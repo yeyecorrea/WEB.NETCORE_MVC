a0f5f3ac4a1958a74f0793e1d576db7223cd1c75
yeyecorrea <yeyecorrea838@gmail.com>
2022-06-15 15:24:51 -0500

    Se agregan las propiedades a las Entidad

--- WEB.NETCORE_MVC/WebAppManageUsers/WebAppManageUsers/Data/Entities/City.cs
+++ WEB.NETCORE_MVC/WebAppManageUsers/WebAppManageUsers/Data/Entities/City.cs
@@ -1,6 +1,16 @@
-﻿namespace WebAppManageUsers.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppManageUsers.Data.Entities
 {
     public class City
     {
+        public int Id { get; set; }
+
+        [Display(Name = "Ciudad")]
+        [MaxLength(20, ErrorMessage = "El campo {0} debe tener maximo {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Name { get; set; }
+
+        public Country Countries { get; set; }
     }
 }
